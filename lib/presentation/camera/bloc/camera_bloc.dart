eaa408c3cc899039f37bd6e42632c678fd22f108
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-07-09 23:16:00 +0700

    CHG - name variable

--- lib/presentation/camera/bloc/camera_bloc.dart
+++ lib/presentation/camera/bloc/camera_bloc.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:foody/service/storage_helper.dart';
+import 'package:foody/service/camera_storage_helper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'camera_page.dart';
@@ -113,7 +113,7 @@ class CameraBloc extends Bloc<CameraEvent, CameraState> {
       ),
     );
     if (file != null) {
-      final saved = await StorageHelper.saveImage(file, 'camera');
+      final saved = await CameraStorageHelper.saveImage(file, 'camera');
       emit(
         (state as CameraReady).copyWith(
           imageFile: saved,
--- lib/service/camera_storage_helper.dart
+++ lib/service/camera_storage_helper.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:path/path.dart' as path;
 
-class StorageHelper {
+class CameraStorageHelper {
   static Future<String> _getFolderPath() async {
     final Directory dir = Directory('/storage/emulated/0/DCIM/FlutterNative');
     if (!await dir.exists()) await dir.create(recursive: true);
