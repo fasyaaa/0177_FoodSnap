b832b1c4322719d0d9b71692b90ac0d8c63eb203
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-07-10 09:15:09 +0700

    ADD - Bloc for login

---
+++ lib/presentation/auth/bloc/login/login_bloc.dart
@@ -0,0 +1,29 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
+import 'package:foody/data/models/request/auth/login_request_model.dart';
+import 'package:foody/data/models/response/auth/login_response_model.dart';
+import 'package:foody/data/repository/auth_repository.dart';
+
+part 'login_event.dart';
+part 'login_state.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final AuthRepository authRepository;
+
+  LoginBloc({required this.authRepository}) : super(LoginInitial()) {
+    on<LoginRequested>(_onLoginRequested);
+  }
+
+  Future<void> _onLoginRequested(
+    LoginRequested event,
+    Emitter<LoginState> emit,
+  ) async {
+    emit(LoginLoading());
+    final result = await authRepository.login(event.requestModel);
+
+    result.fold(
+      (l) => emit(LoginFailure(error: l)),
+      (r) => emit(LoginSuccess(responseModel: r)),
+    );
+  }
+}
