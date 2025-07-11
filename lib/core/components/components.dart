03d417ce421665c9f8dc655dca6eb6cede22d23a
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-07-11 16:11:29 +0700

    ADD - CustomBottomBar function

--- lib/core/components/components.dart
+++ lib/core/components/components.dart
@@ -2,3 +2,4 @@ export 'buttons.dart';
 export 'custom_text_field.dart';
 export 'spaces.dart';
 export 'dropdown_button_2.dart';
+export 'custom_bottom_bar.dart';
---
+++ lib/core/components/custom_bottom_bar.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:foody/core/constants/colors.dart';
+
+class CustomBottomBar extends StatelessWidget {
+  final String currentRoute;
+  final void Function(String route) onTabSelected;
+  final String? profileImageUrl;
+
+  const CustomBottomBar({
+    super.key,
+    required this.currentRoute,
+    required this.onTabSelected,
+    this.profileImageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      decoration: const BoxDecoration(
+        color: AppColors.background,
+        border: Border(top: BorderSide(color: Colors.white10, width: 0.5)),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // Home
+          _buildIconButton(
+            isActive: currentRoute == '/home',
+            activeAsset: 'assets/icons/home_fill.svg',
+            inactiveAsset: 'assets/icons/home_empty.svg',
+            onTap: () => onTabSelected('/home'),
+          ),
+
+          // Location
+          _buildIconButton(
+            isActive: currentRoute == '/location',
+            activeAsset: 'assets/icons/location_fill.svg',
+            inactiveAsset: 'assets/icons/location_empty.svg',
+            onTap: () => onTabSelected('/location'),
+          ),
+
+          // Bookmark
+          _buildIconButton(
+            isActive: currentRoute == '/bookmark',
+            activeAsset: 'assets/icons/bookmark_fill.svg',
+            inactiveAsset: 'assets/icons/bookmark_empty.svg',
+            onTap: () => onTabSelected('/bookmark'),
+          ),
+
+          // Profile with image
+          GestureDetector(
+            onTap: () => onTabSelected('/profile'),
+            child: profileImageUrl != null
+                ? Container(
+                    padding: const EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: currentRoute == '/profile'
+                            ? AppColors.white
+                            : Colors.transparent,
+                        width: 1.5,
+                      ),
+                    ),
+                    child: CircleAvatar(
+                      radius: 13,
+                      backgroundImage: AssetImage(profileImageUrl!),
+                    ),
+                  )
+                : SvgPicture.asset(
+                    currentRoute == '/profile'
+                        ? 'assets/icons/profile_fill.svg'
+                        : 'assets/icons/bookmark_empty.svg', // fallback icon
+                    width: 26,
+                    height: 26,
+                    colorFilter:
+                        const ColorFilter.mode(AppColors.white, BlendMode.srcIn),
+                  ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildIconButton({
+    required bool isActive,
+    required String activeAsset,
+    required String inactiveAsset,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      behavior: HitTestBehavior.opaque,
+      child: SvgPicture.asset(
+        isActive ? activeAsset : inactiveAsset,
+        width: 26,
+        height: 26,
+        colorFilter: const ColorFilter.mode(AppColors.white, BlendMode.srcIn),
+      ),
+    );
+  }
+}
