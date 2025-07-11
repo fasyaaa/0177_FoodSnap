0e8b79c6eccc1ee7b870db5ae0fbc9a3f1b9f07f
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-07-11 08:17:45 +0700

    ADD - api path

---
+++ lib/core/constants/api_path.dart
@@ -0,0 +1,7 @@
+class ApiPath {
+  static const String register = 'auth/register';
+  static const String login = 'auth/login';
+  static const String clients = 'clients';
+  static const String feeds = 'feeds';
+  static const String comments = 'comments';
+}
