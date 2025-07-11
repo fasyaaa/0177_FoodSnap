6c6f0a5c63f5b129a5921895b06325ea897fded6
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-07-11 09:17:24 +0700

    ADD - template admin + client home screen

---
+++ lib/presentation/home/admin_home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AdminHomeScreen extends StatefulWidget {
+  const AdminHomeScreen({super.key});
+
+  @override
+  State<AdminHomeScreen> createState() => _AdminHomeScreenState();
+}
+
+class _AdminHomeScreenState extends State<AdminHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        actions: const [],
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+            children: [],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/home/client_home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ClientHomeScreen extends StatefulWidget {
+  const ClientHomeScreen({super.key});
+
+  @override
+  State<ClientHomeScreen> createState() => _ClientHomeScreenState();
+}
+
+class _ClientHomeScreenState extends State<ClientHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        actions: const [],
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+            children: [],
+        ),
+      ),
+    );
+  }
+}
