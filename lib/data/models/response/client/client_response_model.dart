9a579824d262f036412253311e0e93e42726da3d
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-07-11 12:05:03 +0700

    ADD - image generator for ClientResponseModel

--- lib/data/models/response/client/client_response_model.dart
+++ lib/data/models/response/client/client_response_model.dart
@@ -7,6 +7,7 @@ class ClientResponseModel {
     final String? email;
     final String? gender;
     final String? role;
+    final String? imgProfile;
     final DateTime? createdAtClient;
     final DateTime? updateAtClient;
 
@@ -16,6 +17,7 @@ class ClientResponseModel {
         this.username,
         this.email,
         this.gender,
+        this.imgProfile,
         this.role,
         this.createdAtClient,
         this.updateAtClient,
@@ -31,6 +33,7 @@ class ClientResponseModel {
         username: json["username"],
         email: json["email"],
         gender: json["gender"],
+        imgProfile: json["img_profile"],
         role: json["role"],
         createdAtClient: json["created_at_client"] == null ? null : DateTime.parse(json["created_at_client"]),
         updateAtClient: json["update_at_client"] == null ? null : DateTime.parse(json["update_at_client"]),
@@ -42,6 +45,7 @@ class ClientResponseModel {
         "username": username,
         "email": email,
         "gender": gender,
+        "img_profile": imgProfile,
         "role": role,
         "created_at_client": createdAtClient?.toIso8601String(),
         "update_at_client": updateAtClient?.toIso8601String(),
