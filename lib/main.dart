107f5c4dd7c1b1142b1bdd5becba39492ed7eafc
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-07-12 15:40:14 +0700

    ADD - bloc in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:foody/data/repository/auth_repository.dart';
+import 'package:foody/data/repository/feed_repository.dart';
+import 'package:foody/data/repository/comment_repository.dart';
 import 'package:foody/presentation/auth/bloc/login/login_bloc.dart';
 import 'package:foody/presentation/auth/bloc/register/register_bloc.dart';
 import 'package:foody/presentation/auth/login_screen.dart';
 import 'package:foody/presentation/auth/register_screen.dart';
+import 'package:foody/presentation/home/client/bloc/client_home_bloc.dart';
+import 'package:foody/presentation/home/client/client_home_screen.dart';
 import 'package:foody/service/service_http_client.dart';
 
 void main() {
@@ -16,7 +20,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final authRepository = AuthRepository(ServiceHttpClient());
+    final httpClient = ServiceHttpClient();
+    final authRepository = AuthRepository(httpClient);
+    final feedRepository = FeedRepository(httpClient);
+    final commentRepository = CommentRepository(httpClient);
 
     return MultiBlocProvider(
       providers: [
@@ -26,6 +33,9 @@ class MyApp extends StatelessWidget {
         BlocProvider<RegisterBloc>(
           create: (_) => RegisterBloc(authRepository: authRepository),
         ),
+        BlocProvider<ClientHomeBloc>(
+          create: (_) => ClientHomeBloc(feedRepository, commentRepository)..add(LoadFeeds()),
+        ),
       ],
       child: MaterialApp(
         title: 'FoodSnap',
@@ -33,6 +43,7 @@ class MyApp extends StatelessWidget {
         routes: {
           '/': (context) => const LoginScreen(),
           '/register': (context) => const RegisterScreen(),
+         '/home': (context) => const ClientHomeScreen(),
         },
       ),
     );
