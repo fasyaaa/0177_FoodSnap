9aebdc3537311fd2aa74c066acedf3dcbdf8c8b1
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-07-13 23:05:22 +0700

    ADD - modelling placement

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -5,6 +5,7 @@ import 'package:foody/core/constants/api_path.dart';
 import 'package:foody/data/models/request/auth/login_request_model.dart';
 import 'package:foody/data/models/request/auth/register_request_model.dart';
 import 'package:foody/data/models/response/auth/login_response_model.dart';
+import 'package:foody/data/models/response/auth/register_response_model.dart';
 import 'package:foody/service/service_http_client.dart';
 
 class AuthRepository {
@@ -13,7 +14,9 @@ class AuthRepository {
 
   AuthRepository(this._serviceHttpClient);
 
-  Future<Either<String, LoginResponseModel>> login(LoginRequestModel requestModel) async {
+  Future<Either<String, LoginResponseModel>> login(
+    LoginRequestModel requestModel,
+  ) async {
     try {
       final response = await _serviceHttpClient.post(
         ApiPath.login,
@@ -23,9 +26,12 @@ class AuthRepository {
       final jsonResponse = json.decode(response.body);
       if (response.statusCode == 200) {
         final loginResponse = LoginResponseModel.fromMap(jsonResponse);
-        await secureStorage.write(key: "authToken", value: loginResponse.data!.token);
-        await secureStorage.write(key: "UserRole", value: loginResponse.data!.role);
-        await secureStorage.write(key: "clientId", value: loginResponse.data!.id.toString());
+        await secureStorage.write(
+            key: "authToken", value: loginResponse.data!.token);
+        await secureStorage.write(
+            key: "UserRole", value: loginResponse.data!.role);
+        await secureStorage.write(
+            key: "clientId", value: loginResponse.data!.id.toString());
         return Right(loginResponse);
       } else {
         return Left(jsonResponse['message'] ?? "Login failed");
@@ -35,7 +41,9 @@ class AuthRepository {
     }
   }
 
-  Future<Either<String, String>> register(RegisterRequestModel requestModel) async {
+  Future<Either<String, RegisterResponseModel>> register(
+    RegisterRequestModel requestModel,
+  ) async {
     try {
       final response = await _serviceHttpClient.post(
         ApiPath.register,
@@ -44,7 +52,22 @@ class AuthRepository {
 
       final jsonResponse = json.decode(response.body);
       if (response.statusCode == 201) {
-        return Right(jsonResponse['message']);
+        final registerResponse = RegisterResponseModel.fromMap(jsonResponse);
+
+        if (registerResponse.data?.token != null &&
+            registerResponse.data?.id != null) {
+          await secureStorage.write(
+              key: "authToken", value: registerResponse.data!.token);
+          await secureStorage.write(
+              key: "UserRole", value: registerResponse.data!.role);
+          await secureStorage.write(
+              key: "clientId", value: registerResponse.data!.id.toString());
+          return Right(registerResponse);
+        } else {
+          return const Left(
+            "Registration response is missing token or user data.",
+          );
+        }
       } else {
         return Left(jsonResponse['message'] ?? "Registration failed");
       }
