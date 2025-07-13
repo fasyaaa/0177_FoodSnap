446b27a7c2432e0676aed28cc9daf902a92c7ffa
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-07-13 12:27:49 +0700

    ADD - bloc for profile page

--- lib/presentation/profile/client/bloc/profile_bloc.dart
+++ lib/presentation/profile/client/bloc/profile_bloc.dart
@@ -1,13 +1,49 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:foody/data/models/response/feeds/feed_response_model.dart';
+import 'package:foody/data/repository/client_repository.dart';
+import 'package:foody/data/repository/feed_repository.dart';
 
 part 'profile_event.dart';
 part 'profile_state.dart';
 
 class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
-  ProfileBloc() : super(ProfileInitial()) {
-    on<ProfileEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final ClientRepository clientRepository;
+  final FeedRepository feedRepository;
+
+  ProfileBloc({
+    required this.clientRepository,
+    required this.feedRepository,
+  }) : super(ProfileInitial()) {
+    on<LoadProfile>(_onLoadProfile);
+  }
+
+  Future<void> _onLoadProfile(LoadProfile event, Emitter<ProfileState> emit) async {
+    emit(ProfileLoading());
+
+    try {
+      final clientResult = await clientRepository.getClientById(event.clientId);
+      final feedResult = await feedRepository.getAllFeeds();
+
+      return clientResult.fold(
+        (error) => emit(ProfileError(error)),
+        (client) {
+          // Filter feeds by current client ID
+          final userFeeds = feedResult.fold(
+            (error) => <FeedsResponseModel>[],
+            (feeds) => feeds.where((f) => f.idClient == client.idClient).toList(),
+          );
+
+          emit(ProfileLoaded(
+            username: client.username,
+            name: client.name,
+            imgProfile: client.imgProfile,
+            feeds: userFeeds,
+          ));
+        },
+      );
+    } catch (e) {
+      emit(ProfileError('Failed to load profile: $e'));
+    }
   }
 }
--- lib/presentation/profile/client/bloc/profile_event.dart
+++ lib/presentation/profile/client/bloc/profile_event.dart
@@ -4,5 +4,14 @@ sealed class ProfileEvent extends Equatable {
   const ProfileEvent();
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
+}
+
+class LoadProfile extends ProfileEvent {
+  final int clientId;
+
+  const LoadProfile(this.clientId);
+
+  @override
+  List<Object?> get props => [clientId];
 }
